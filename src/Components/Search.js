f3df9c1faebbbe75452c675dc9232312fec485bf
Ishan Jain <ishanjaincool81@gmail.com>
2022-11-17 19:40:17 +0530

    Make Filter Bar in Search Page

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -7,9 +7,12 @@ import CircularProgress from '@mui/material/CircularProgress';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
 import { Link } from '@mui/material';
 import ReactTimeAgo from 'react-time-ago';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 class Search extends Component {
 constructor() {
@@ -20,17 +23,91 @@ constructor() {
         currPage: 1,
         parr:[1],
         loading: true,
+        tag: "story",
+        sort: "Popularity",
+        timeRange: "All Time"
     };
 }
 
     async componentDidMount(){
-        const res = await axios.get(`https://hn.algolia.com/api/v1/search?query=`);
+        const res = await axios.get(`https://hn.algolia.com/api/v1/search?query=&tags=${this.state.tag}&page=${this.state.currPage-1}&hitsPerPage=30`);
         let data = res.data;
         this.setState({
             news: [...data.hits],
             loading: false
         })
         console.log(data.hits[0]);
+    }
+
+    changeNews = async () => {
+        const res = await axios.get(`https://hn.algolia.com/api/v1/search?query=&tags=${this.state.tag}&page=${this.state.currPage-1}&hitsPerPage=30`);
+        let data = res.data;
+        this.setState({
+            news: [...data.hits],
+            loading: false
+        })
+    }
+
+    handleRight = () => {
+        if(this.state.currPage === this.state.parr.length){
+            let temparr = [];
+            for(let i=1;i<=this.state.parr.length+1;i++){
+                temparr.push(i);
+            }
+            this.setState({
+                parr : [...temparr],
+                currPage : this.state.currPage+1,
+                loading: true
+            },this.changeNews)
+        }
+        else{
+            this.setState({
+                currPage : this.state.currPage+1,
+                loading: true
+            },this.changeNews)
+        }
+    }
+    handleLeft = () =>{
+        if(this.state.currPage !== 1){
+            this.setState({
+                currPage : this.state.currPage - 1,
+                loading: true  
+            },this.changeNews)
+        }
+    }
+
+    handleClick = (value) =>{
+        if(value !== this.state.currPage){
+            this.setState({
+                currPage : value,
+                loading: true
+            },this.changeNews)
+        }
+    }
+
+    handleTagFilter = (event) => {
+        let value = event.target.value;
+        if(value === "All"){
+            value = "";
+        }
+        this.setState({
+            tag: value,
+            loading: true,
+            currPage: 1,
+            parr:[1],
+        },this.changeNews)
+    };
+
+    handleSortFilter = (event) => {
+        this.setState({
+            sort: event.target.value
+        })
+    }
+
+    handleTimeRange = (event) =>{
+        this.setState({
+            timeRange: event.target.value
+        })
     }
 
     render() {
@@ -42,6 +119,48 @@ constructor() {
                         <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass}/>
                         <TextField id="input-with-sx" label="Search Stories by Title, URL or Author" variant="outlined" style={{width: '80%'}} />
                     </div>
+                    <div className="filter-bar">
+                        <p style={{marginLeft: '0.7%'}}>Search</p>
+                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                            <InputLabel id="demo-select-small"></InputLabel>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={this.state.tag === "" ? "All" : this.state.tag}
+                                onChange={this.handleTagFilter}
+                            >
+                                <MenuItem value={"All"}>All</MenuItem>
+                                <MenuItem value={"story"}>Stories</MenuItem>
+                                <MenuItem value={"comment"}>Comments</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <p>by</p>
+                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                            <InputLabel id="demo-select-small"></InputLabel>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={this.state.sort}
+                                onChange={this.handleSortFilter}
+                            >
+                                <MenuItem value={"Popularity"}>Popularity</MenuItem>
+                                <MenuItem value={"Date"}>Date</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <p>for</p>
+                        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                            <InputLabel id="demo-select-small"></InputLabel>
+                            <Select
+                                labelId="demo-select-small"
+                                id="demo-select-small"
+                                value={this.state.timeRange}
+                                onChange={this.handleTimeRange}
+                            >
+                                <MenuItem value={"All Time"}>All Time</MenuItem>
+                                <MenuItem value={"Last 24H"}>Last 24H</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
                     {
                         this.state.loading === true ? 
                         <CircularProgress/>:
@@ -50,12 +169,6 @@ constructor() {
                                 this.state.news.map((element,index)=>{
                                     return(
                                         <ListItem key={element.objectID} sx={{paddingTop: 0, paddingBottom: 0}}>
-                                            <ListItemAvatar sx={{minWidth: 20, padding: 0, paddingRight: 1}}>
-                                                {
-                                                    ((this.state.currPage-1) * 30 + index + 1)
-                                                }
-                                                .
-                                            </ListItemAvatar>
                                             <ListItemText primary={
                                                 <Link sx={{lineHeight: 'auto'}} underline="none" href={element.url} style={{cursor:'pointer', color: 'black'}}>{element.title}</Link>} 
                                                 secondary={
@@ -70,6 +183,22 @@ constructor() {
                             }
                         </List>
                     }
+                    <div style={{display:'flex',justifyContent:'center'}}>
+                        <nav aria-label="Page navigation example">
+                            <ul className="pagination">
+                                <li className="page-item"><a className="page-link" onClick={this.handleLeft} style={{cursor:'pointer'}}>Previous</a></li>
+                                {
+                                    this.state.parr.map((value)=>(
+                                        value === this.state.currPage ? 
+                                        <li className="page-item active" key={value}><a className="page-link" onClick={()=>this.handleClick(value) } style={{cursor:'pointer'}}>{value}</a></li>
+                                        :
+                                        <li className="page-item" key={value}><a className="page-link" onClick={()=>this.handleClick(value) } style={{cursor:'pointer'}}>{value}</a></li>
+                                    ))
+                                }
+                                <li className="page-item"><a className="page-link" onClick={this.handleRight} style={{cursor:'pointer'}}>Next</a></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </div>
         )
